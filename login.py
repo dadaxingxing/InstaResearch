de1647bbcce6580df125504ef4ee634c17354fe4
Joey <xiaxing.zhu16@gmail.com>
2024-06-27 18:31:30 -0700

    process.py and made stuff work

---
+++ login.py
@@ -0,0 +1,19 @@
+def login_user(check, USERNAME, PASSWORD):
+    cl = Client()
+    try:
+        if check:
+            cl.load_settings('session.json')
+            cl.login(USERNAME, PASSWORD)
+            cl.get_timeline_feed()
+            # except Exception as e:
+            #     cl.login(USERNAME, PASSWORD)
+            #     cl.dump_settings('session.json')
+        else:
+            cl.login(USERNAME, PASSWORD)
+            cl.dump_settings('session.json')
+
+        return cl
+    
+    except Exception as e:
+        print('Could not login, error occured.')  
+        return None
--- main.py
+++ main.py
@@ -1,30 +1,11 @@
 from instagrapi import Client
 from dotenv import dotenv_values
+from login import login_user
 
-
-def login_user(check, USERNAME, PASSWORD):
-    cl = Client()
-    try:
-        if check:
-            cl.load_settings('session.json')
-            cl.login(USERNAME, PASSWORD)
-            cl.get_timeline_feed()
-            # except Exception as e:
-            #     cl.login(USERNAME, PASSWORD)
-            #     cl.dump_settings('session.json')
-        else:
-            cl.login(USERNAME, PASSWORD)
-            cl.dump_settings('session.json')
-
-        return cl
-    
-    except Exception as e:
-        print('Could not login, error occured.')  
-        return None
 
 config = dotenv_values('login.env')
 USERNAME = config['USERNAME']
 PASSWORD = config['PASSWORD']
 
-print(USERNAME, PASSWORD)
-# cl = login_user(True, )
+cl = login_user(True, USERNAME, PASSWORD)
+print(cl.user_medias_gql('10642672448', 4, 2))
