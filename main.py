880518c1f8246b5e87720410a046d06e8fed995f
Coson Lu <coson.lu271@gmail.com>
2024-06-28 17:44:42 -0700

    Formated the printing

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ cl = login_user(True, USERNAME, PASSWORD)
 USER_USERNAME = input('Enter the instagram username: ')
 USER_ID = cl.user_info_by_username_v1(USER_USERNAME)
 
-data = cl.user_medias_gql(USER_ID.pk, 20, 2)
-print(process_info(data))
+data = cl.user_medias_gql(USER_ID.pk, 100, 2)
+processed = process_info(data)
+for i in range(len(processed)):
+    print(f'{i + 1}. Link: {processed[i]['Link']}, Likes: {processed[i]['Likes']}')
